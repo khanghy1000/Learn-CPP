7dd48a31c3b029ccd38c654c2ae310562f1ab927
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2020-04-01 22:10:18 +0700

    Update random 1 số (chạy nhiều lần).cpp

--- random 1 số (chạy nhiều lần).cpp
+++ random 1 số (chạy nhiều lần).cpp
@@ -1,6 +1,11 @@
+/* Chương trình random số
+   Nhập 2 số a và b,
+   Chương trình sẽ random 1 số trong khoảng a và b */
+
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <cstdlib> // Dùng cho hàm rand
+#include <ctime> // Dùng cho hàm time
+
 using namespace std;
 
 int main() {
@@ -13,18 +18,17 @@ int main() {
 		cout << "Nhap a = "; cin >> a;
 		cout << "Nhap b = "; cin >> b;
 
-		srand(time(NULL));
+		srand(time(NULL)); // Random 1 số bằng thời gian
 
 		if (a < b) {
-			random = rand() % (b - a + 1) + a;
+			random = rand() % (b - a + 1) + a; // Lấy giá trị random chia dư (b-a+1) rồi cộng a để có giá trị trong khoảng a và b (a<b)
 		}
 
 		else {
-			random = rand() % (a - b + 1) + b;
+			random = rand() % (a - b + 1) + b; // Lấy giá trị random chia dư (a-b+1) rồi cộng b để có giá trị trong khoảng a và b (a>b)
 		}
 
-		cout << "So random la " << random << endl << endl;
-
+		cout << "So random la " << random << endl;
 		cout << "Ban co muon tiep tuc khong? " << endl;
 		cout << "1: Co" << endl << "2: Khong" << endl << "Chon: "; cin >> choose;
 
